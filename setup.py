58f284587f67fd1b448d821407ae752c10f8e4d9
Robert Lieck <robert.lieck@durham.ac.uk>
2024-03-07 03:07:22 +0000

    version 0.0.5

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt", "r") as fh:
 
 setuptools.setup(
     name="muprocdurham",
-    version="0.0.4",
+    version="0.0.5",
     author="Robert Lieck, Eamonn Bell, Mark Gotham",
     # author_email="",
     description="Python package for the Music Processing lecture and practicals at Durham University",
